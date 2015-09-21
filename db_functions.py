4a3178465e064023af87a60b9df257857f8bedca
adenguo <adenguo@gmail.com>
2015-09-21 21:14:51 +0800

    Finish Part 2

--- db_functions.py
+++ db_functions.py
@@ -11,6 +11,7 @@ import datetime
 
 JSON_FILE = '../beijing_china.osm.json'
 DB_NAME = 'examples'
+TIME_FREQUENCY_PNG = 'figures/updating_frequency.png'
 time_reg = re.compile(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$')
 
 
@@ -29,17 +30,15 @@ def get_collections(db_name):
 
 def make_pipeline():
     # complete the aggregation pipeline
-    pipeline = [{'$match':{'country':'India', 'lon':{'$gte':75}, 'lon':{'$lte': 80}}},
-                {'$unwind':'$isPartOf'},
-                {'$group':{'_id':'$isPartOf',
+    pipeline = [{'$group':{'_id':'$timestamp',
                            'count':{'$sum':1}}},
                 {'$sort':{'count':-1}},
                 {'$limit':1}
                 ]
     return pipeline
 
-def aggregate(db, pipeline):
-    result = db.cities.aggregate(pipeline)
+def aggregate(collection, pipeline):
+    result = collection.aggregate(pipeline)
     return result
 
 def insert_maps(infile, db_name):
@@ -55,8 +54,30 @@ def process_file(filename):
             result.append(json.loads(line.strip()))
     return result
 
-def get_unique_time(collection):
+def get_unique_time_sorted(collection):
     time_string_list = collection.distinct('timestamp')
-    result = [datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ') for \
+    result = [datetime.datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ') for \
               date_string in time_string_list]
+    result.sort()
     return result
+
+def get_all_timestamp(collection):
+    return [datetime.datetime.strptime(string['timestamp'], \
+                   '%Y-%m-%dT%H:%M:%SZ') for string in \
+                   list(collection.find({}, {'timestamp': 1, '_id':0}))]
+
+def plot_timestamp_png(collection,png_filename):
+    data = get_all_timestamp(collection)
+    import matplotlib.pyplot as plt
+    import matplotlib.dates as mdates
+    mpl_data = mdates.date2num(data)
+    fig, ax = plt.subplots(1,1)
+    ax.hist(mpl_data, bins=20, color='lightblue')
+    ax.xaxis.set_major_locator(mdates.YearLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%y.%m'))
+    fig.savefig(png_filename, dpi=1000)    
+
+if __name__ == '__main__':
+    all_collections = get_collections(DB_NAME)
+    unique_times = get_unique_time_sorted(all_collections)
+    plot_timestamp_png(all_collections,TIME_FREQUENCY_PNG)
